f1ea6ac50005f5d13840d79633954fff5cfd961f
gashishishi <wawwooaz@gmail.com>
2023-03-09 10:16:32 +0900

    公開用

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/sass/app.scss',
-                'resources/css/style.scss',
+                'resources/css/style.css',
                 'resources/js/app.js',
                 'resources/js/list.js',
                 'resources/js/nav-bar.js',
